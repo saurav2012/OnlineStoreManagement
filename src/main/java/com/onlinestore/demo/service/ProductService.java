c57262370df98ce1c19cbf288c28d148dcbb66bf
Shresta M <71119133+shresta-m@users.noreply.github.com>
2023-05-22 23:25:39 +0530

    added productService code

---
+++ src/main/java/com/onlinestore/demo/service/ProductService.java
@@ -0,0 +1,49 @@
+package com.onlinestore.demo.service;
+
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import com.example.demo.model.Product;
+import com.example.demo.repository.ProductRepository;
+
+@Service
+public class ProductService{
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    public List<Product> getAll(){
+        return productRepository.findAll();
+    }
+
+    public Product getProductById(Long id){
+        Optional<Product> product = productRepository.findById(id);
+        if(product.isPresent()){
+            return product.get();
+        }
+        //throw new ProductNotFoundException("Product with id "+ id+" not present");
+        return null;
+    }
+
+    public Product save(Product product){
+        return productRepository.save(product);
+    }
+
+    public Product update(Product product){
+        Optional<Product> data = productRepository.findById(product.getId());
+        if(data.isEmpty()){
+            //throw new ProductNotFoundException("Product with id "+ id+" not present. So update cannot be performed");
+        }
+        return productRepository.save(product);
+    }
+
+    public void deleteProductById(Long id){
+        Optional<Product> data = productRepository.findById(id);
+        if(data.isEmpty()){
+            //throw new ProductNotFoundException("Product with id "+ id+" not present. So update cannot be performed");
+        }
+        productRepository.deleteById(id);;
+    }
+    
+}
